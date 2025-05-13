ebeba0cfbae64d78647b9f65cb3481d65638db9f
dacunaqmdsi <dacuna@qmdsi.com>
2025-05-13 17:54:17 +0800

    Save it

---
+++ piggery.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.5.5-10.4.32-MariaDB : Database - piggery
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `tblaccounts` */
+
+DROP TABLE IF EXISTS `tblaccounts`;
+
+CREATE TABLE `tblaccounts` (
+  `accountid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) DEFAULT '',
+  `account_password` varchar(128) DEFAULT '',
+  `account_type` varchar(128) DEFAULT '',
+  `is_blocked` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`accountid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tblaccounts` */
+
+insert  into `tblaccounts`(`accountid`,`username`,`account_password`,`account_type`,`is_blocked`) values (1,'darren','darren','Admin',0),(2,'Kelllyssss','Kelly','Farm Owner',0),(3,'DA','DA','Farm Owner',0);
+
+/*Table structure for table `tblcustomer` */
+
+DROP TABLE IF EXISTS `tblcustomer`;
+
+CREATE TABLE `tblcustomer` (
+  `customerid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `actions` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`customerid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tblcustomer` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
